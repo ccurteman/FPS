1407c4f21f4a4998995f9b95cbfea5e35436e331
unknown <c.curteman@gmail.com>
2020-05-21 11:20:50 -0500

    Initial Codebase

---
+++ entities/zombie/Zombie.gd
@@ -0,0 +1,48 @@
+extends KinematicBody
+ 
+const MOVE_SPEED = 3
+ 
+onready var raycast = $RayCast
+onready var anim_player = $AnimationPlayer
+ 
+var player = null
+var dead = false
+ 
+func _ready():
+	anim_player.play("walk")
+	add_to_group("zombies")
+ 
+
+func _physics_process(delta):
+	if dead:
+		return
+	if player == null:
+		anim_player.stop()
+		$Sprite3D.frame = 0
+		return
+		
+	anim_player.play("walk")
+   
+	var vec_to_player = player.translation - translation
+	vec_to_player = vec_to_player.normalized()
+	raycast.cast_to = vec_to_player * 1.5
+	
+	vec_to_player.y = -10
+   
+	move_and_collide(vec_to_player * MOVE_SPEED * delta)
+   
+	if raycast.is_colliding():
+		var coll = raycast.get_collider()
+		if coll != null and coll.name == "Player":
+			coll.kill()
+   
+ 
+func kill():
+	dead = true
+	$DetectionArea.monitoring = false
+	$DetectionArea/CollisionShape.disabled = true
+	$CollisionShape.disabled = true
+	anim_player.play("death")
+ 
+func set_player(p):
+	player = p
